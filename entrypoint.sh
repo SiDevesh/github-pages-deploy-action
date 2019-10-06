74517979cc41dab95fb37cf87a8a044d6705b2c4
SiDevesh <sid.swapnildevesh@gmail.com>
2019-10-06 14:35:41 +0530

    Allow Repository to be configured

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,12 @@ then
   exit 1
 fi
 
+# If the REPOSITORY is not given then use the current repository
+if [ -z "$REPOSITORY" ]
+then
+  REPOSITORY="${GITHUB_REPOSITORY}"
+fi
+
 case "$FOLDER" in /*|./*)
   echo "The deployment folder cannot be prefixed with '/' or './'. Instead reference the folder name directly."
   exit 1
@@ -54,7 +60,7 @@ git config --global user.email "${COMMIT_EMAIL}" && \
 git config --global user.name "${COMMIT_NAME}" && \
 
 ## Initializes the repository path using the access token.
-REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git" && \
+REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${REPOSITORY}.git" && \
 
 # Checks to see if the remote exists prior to deploying.
 # If the branch doesn't exist it gets created here as an orphan.
