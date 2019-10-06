db79ba5e8bcca3475e96839670a635ba49feabe5
SiDevesh <sid.swapnildevesh@gmail.com>
2019-10-06 16:26:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,6 +69,7 @@ fi
 
 if [ -n "$DEPLOY_KEY" ] && [ -n "$DEPLOY_KEY_PUB" ]
 then
+  mkdir ~/.ssh
   echo $DEPLOY_KEY > ~/.ssh/id_rsa && \
   echo $DEPLOY_KEY_PUB > .ssh/id_rsa.pub && \
   eval "$(ssh-agent -s)" && \
